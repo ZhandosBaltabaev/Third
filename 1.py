c6e0773cb9a1f958ccc0415db8cb5230d6447898
ZhandosBaltabaev <90600100+ZhandosBaltabaev@users.noreply.github.com>
2021-10-29 19:38:16 +0600

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,114 @@
+import math
+a=10
+b=-5
+c=4.3
+d=3
+print('a=',a)
+print('b=',b)
+print('c=',c)
+print('d=',d)
+z=math.ceil(a)
+print('math.ceil(',c,') =',z)
+z=math.fabs(b)
+print('math.fabs(',b,') =',z)
+z=math.factorial(a)
+print('math.factorial(',a,') =',z)
+z=math.floor(c)
+print('math.floor(',c,') =',z)
+z=math.exp(b)
+print('math.exp(',b,') =',z)
+z=math.log2(a)
+print('math.log2(',a,') =',z)
+z=math.log10(a)
+print('math.log10(',a,') =',z)
+z=math.log(d,a)
+print('math.log(',d,',',a,') =',z)
+z=math.pow(a,d)
+print('math.pow(',a,',',d,') =',z)
+z=math.sqrt(a)
+print('math.sqrt(',a,') =',z)
+
+
+
+import math
+x=1
+print('x=',x)
+
+z=math.cos(x)
+print('math.cos(',x,') =',z)
+
+z=math.sin(x)
+print('math.sin(',x,') =',z)
+
+z=math.tan(x)
+print('math.tan(',x,') =',z)
+
+z=math.acos(x)
+print('math.acos(',x,') =',z)
+
+z=math.asin(x)
+print('math.asin(',x,') =',z)
+
+z=math.atan(x)
+print('math.atan(',x,') =',z)
+
+
+
+import math
+x=int(input("Введите переменную x:"))
+t=int(input("Введите переменную t:"))
+z=((9*math.pi*t+10*math.cos(x))/(math.sqrt(t)-math.fabs(math.sin(t))))*math.pow(math.e,x)
+print("z = {0:.2f}".format(z))
+
+
+number= 5#int
+
+digit=-4.54356876#float
+word="Результат:"#string
+boolean=True#bool
+
+str_num='5'#string
+
+#print(word+str(digit))
+
+print(word+str(number+int(str_num)))
+
+del number
+
+number=7
+print("Результат:",number)
+
+
+
+
+num1 = int(input("Введите первое число: "))
+
+num2 = int(input("Введите второе число: "))
+
+num1 *= 5
+
+print("Result:", num1 + num2)
+print("Result:", num1 - num2)
+print("Result:", num1 / num2)
+print("Result:", num1 * num2)
+print("Result:", num1 ** num2)
+print("Result:", num1 // num2)
+
+word = "Hi"
+print(word * 2)
+
+word = True
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
---
+++ 4.py
@@ -0,0 +1,12 @@
+C[]
+for i in rage (-6):
+    c.append(i*5)
+
+print(c)
+l=[]
+l.append(5)
+l.append(6)
+print(e)
+l.extend(c)
+lsort()
+print(1)
